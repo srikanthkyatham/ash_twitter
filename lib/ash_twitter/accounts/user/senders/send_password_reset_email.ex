649d53dd204c632beb3d1b2611f7bb1ceecd051f
Prasanna_emmadi <prasanna.177@gmail.com>
2023-11-15 13:01:24 +0200

    chore!: adding reset password with email

---
+++ lib/ash_twitter/accounts/user/senders/send_password_reset_email.ex
@@ -0,0 +1,15 @@
+defmodule AshTwitter.Accounts.User.Senders.SendPasswordResetEmail do
+  @moduledoc """
+  Sends a password reset email
+  """
+  use AshAuthentication.Sender
+  use AshTwitterWeb, :verified_routes
+
+  @impl AshAuthentication.Sender
+  def send(user, token, _) do
+    AshTwitter.Accounts.Emails.deliver_reset_password_instructions(
+      user,
+      url(~p"/password-reset/#{token}")
+    )
+  end
+end
